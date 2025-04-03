0f40cb83deaafb4c4b9e73e9595109d24936d9b3
Aaron Perez <aaperez06@gmail.com>
2025-04-03 05:23:01 +0000

    config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+
+  devIndicators: false,
+  transpilePackages: ['@view360/core', '@view360/controls'],
 };
 
 export default nextConfig;
